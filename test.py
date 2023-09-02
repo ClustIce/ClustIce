3a9cbb1bf7a885b8d47c5735f60c5d7486a999c2
vitroid <vitroid@gmail.com>
2023-09-02 13:26:35 +0900

    Refinements.

--- test.py
+++ test.py
@@ -5,34 +5,38 @@ import numpy as np
 
 from clustice.coder import decode, encode
 from clustice.geometry import make_layout
-from clustice.graph import great_icosahedron
+from clustice.graph import great_icosahedron, great_decahedron
 from clustice.gromacs import render
 from clustice.topology import ice_graph
 from clustice.water import tip4p
 
 logger = getLogger()
-basicConfig(level=DEBUG)
+basicConfig(level=INFO)
 
 np.random.seed(12345)
 
 # g = nx.dodecahedral_graph()  # dodecahedral 20mer
 # g = nx.cubical_graph()
-g = great_icosahedron(2)
+# g = great_icosahedron(2)
+g = great_decahedron(20)
 
 # O-O distance
 L = 0.27
 
-layout = make_layout(g, edgelen=L)
+# estimate of the positions of the nodes
+# layout = make_layout(g, edgelen=L)
+# or, extract the embedded coords in g
+layout = np.array([g.nodes[v]["pos"] for v in g])
 
 # set orientations of the hydrogen bonds.
 # if pos is given, the net dipole moment is minimized.
 dg = ice_graph(g, pos=layout)
-# dg = ice_graph(g)
-id = encode(dg)
-print(id)
-# dg2 = decode(id)
-# print(encode(dg2))
-# assert nx.is_isomorphic(dg, dg2)
+
+test_encoder = False
+if test_encoder:
+    id = encode(dg)
+    dg2 = decode(id)
+    assert nx.is_isomorphic(dg, dg2)
 
 
 # put water molecules
